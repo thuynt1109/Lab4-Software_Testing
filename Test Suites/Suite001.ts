15f17eb31691fd4e2bbe9480b11850dc78762035
thuynt <thuybinh11997@gmail.com>
2020-10-08 14:23:34 +0700

    first commit

---
+++ Test Suites/Suite001.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3cb21545-54cc-4186-9a08-6da10d063574</testSuiteGuid>
+   <testCaseLink>
+      <guid>14a576f4-d7de-4356-8739-b0a241bfcab9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_profile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
